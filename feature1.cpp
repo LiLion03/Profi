4e5ca2baf8d2b78ba89f99f200a889f83723d351
Ignatchenko Lev <headhunter16@bk.ru>
2022-08-16 18:20:19 +0300

    Added feature1.cpp (experimental)

---
+++ feature1.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main()
+{
+	string name;
+	cout << "Enter your name, please: " << endl;
+	cin >> name;
+	cout << "Nice to meet you, ", name;
+	return 0;
+}
